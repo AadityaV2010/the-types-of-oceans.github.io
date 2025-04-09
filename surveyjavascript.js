0c8a15c81bc5e33b5237307a0749c210640abe7d
Aaditya Visavadiya <134105512+AadityaV2010@users.noreply.github.com>
2025-04-09 19:13:13 -0400

    Create surveyjavascript.js

---
+++ surveyjavascript.js
@@ -0,0 +1,8 @@
+function surveyformsubmitbuttonFunction() {
+  /*javascript: */document.getElementById('surveydiv').style.display = 'initial';
+  /*javascript: */document.getElementById('surveyformfieldset').style.display = 'none';
+  /*javascript: */document.getElementById('inputtypebuttonsurvey').style.display = 'none';
+  document.getElementById('surveyformreloadbutton').style.display = 'initial';
+}
+
+//Need to make cookies for survey form just like the contact us form
